d5e667594e8254293c5126154b804c65a1edef2e
jackflaggg <rasul.khamzinnn@gmail.com>
2024-09-18 23:35:57 +0500

    init app

--- src/commands/ffmpeg/ffmpeg.builder.ts
+++ src/commands/ffmpeg/ffmpeg.builder.ts
@@ -1,21 +1,33 @@
 export class ffmpegBuilder {
     private inputPath: string;
-    private outputPath: string;
     private options: Map<string, string> = new Map();
 
     constructor (){
         this.options.set('-c:v', 'libx264');
     }
 
-    input(inputPath: string){
+    input(inputPath: string): this{
         this.inputPath = inputPath;
+        return this;
     }
 
-    setVideoSize(width: number, height: number){
-
+    setVideoSize(width: number, height: number): this{
+        this.options.set('-s', `${width}x${height}`);
+        return this;
     }
 
-    output(outputPath: string){
-        this.outputPath = outputPath;
+    output(outputPath: string): string[]{
+        if (!this.inputPath) {
+            throw new Error('Не задан параметр input!');
+        }
+
+        const args: string[] = ['-i', this.inputPath];
+
+        this.options.forEach((value, key) => {
+            args.push(key);
+            args.push(value);
+        });
+        args.push(outputPath);
+        return args;
     }
 }
