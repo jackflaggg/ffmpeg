0b743519c3daa67d4139a1ac5455714588eada79
jackflaggg <rasul.khamzinnn@gmail.com>
2024-09-18 22:31:48 +0500

    init app

--- src/core/prompt/prompt.service.ts
+++ src/core/prompt/prompt.service.ts
@@ -1,9 +1,10 @@
 import inquirer from 'inquirer'
+import {PromptType} from "../../models/common-models";
 
 export class PromptService {
-    public async input(msg: string, ): Promise<any> {
-        const data = inquirer.prompt([{
-            type: '',
+    public async input(msg: string, type: PromptType) {
+        const data = await inquirer.prompt([{
+            type,
             name: 'result',
             message: 'What is the project name?',
         }])
