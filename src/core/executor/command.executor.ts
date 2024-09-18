ecc0d3aa917f4478a6fe2897be87325bf5991a88
jackflaggg <rasul.khamzinnn@gmail.com>
2024-09-18 23:20:47 +0500

    init app

--- src/core/executor/command.executor.ts
+++ src/core/executor/command.executor.ts
@@ -1,5 +1,6 @@
 import {IStreamLogger} from "../handlers/stream.logger.interface";
 import {ChildProcessWithoutNullStreams} from "node:child_process";
+import {ICommandExecute} from "../../models/common-models";
 
 export abstract class CommandExecutor<Input> {
     constructor(private logger: IStreamLogger){};
@@ -14,6 +15,6 @@ export abstract class CommandExecutor<Input> {
 
     protected abstract prompt(): Promise<Input>;
     protected abstract build(input: Input): any;
-    protected abstract spawn(command: any): ChildProcessWithoutNullStreams;
+    protected abstract spawn(command: ICommandExecute): ChildProcessWithoutNullStreams;
     protected abstract processStream(stream: ChildProcessWithoutNullStreams, logger: IStreamLogger): void;
 }
