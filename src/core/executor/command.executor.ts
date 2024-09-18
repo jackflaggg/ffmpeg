f198fb19a01270a86a382e4fa5e50c42b5ef556d
jackflaggg <rasul.khamzinnn@gmail.com>
2024-09-18 23:18:09 +0500

    init app

--- src/core/executor/command.executor.ts
+++ src/core/executor/command.executor.ts
@@ -8,12 +8,12 @@ export abstract class CommandExecutor<Input> {
         const input = await this.prompt();
         const command = this.build(input);
         const stream = this.spawn(command);
-        this.processStream(stream);
+        this.processStream(stream, this.logger);
     };
 
 
     protected abstract prompt(): Promise<Input>;
     protected abstract build(input: Input): any;
     protected abstract spawn(command: any): ChildProcessWithoutNullStreams;
-    protected abstract processStream(stream: ChildProcessWithoutNullStreams): void;
+    protected abstract processStream(stream: ChildProcessWithoutNullStreams, logger: IStreamLogger): void;
 }
