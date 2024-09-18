254226883d51838d258eb903c68a8987a379bf19
jackflaggg <rasul.khamzinnn@gmail.com>
2024-09-18 22:49:56 +0500

    init app

--- src/core/handlers/stream.handler.ts
+++ src/core/handlers/stream.handler.ts
@@ -1,8 +1,20 @@
 import {IStreamLogger} from "./stream.logger.interface";
-import {ChildProcess, ChildProcessWithoutNullStreams} from "node:child_process";
+import {ChildProcessWithoutNullStreams} from "node:child_process";
 
 export class StreamHandler {
     constructor(private outHandler: IStreamLogger) {}
 
-    processOutput(stream: ChildProcessWithoutNullStreams){}
+    processOutput(stream: ChildProcessWithoutNullStreams){
+        stream.stdout.on('data', (data: any) => {
+            this.logger.log(data)
+        })
+
+        stream.stdder.on('data', (data: any) => {
+            this.logger.error(data)
+        })
+
+        stream.on('close', () => {
+            this.logger.end()
+        })
+    }
 }
