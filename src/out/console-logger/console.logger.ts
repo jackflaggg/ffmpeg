fa8a1d76fe0bcc90faeb47b03c2598575c399f64
jackflaggg <rasul.khamzinnn@gmail.com>
2024-09-18 23:00:22 +0500

    init app

--- src/out/console-logger/console.logger.ts
+++ src/out/console-logger/console.logger.ts
@@ -1,18 +1,25 @@
 import {IStreamLogger} from "../../core/handlers/stream.logger.interface";
 
-export class ConsoleLogger {
-    private static instance: ConsoleLogger;
+export class ConsoleLogger implements IStreamLogger {
+    private static logger: ConsoleLogger;
 
     private constructor() {}
 
     private static createSingleton(){
-        if(!this.instance){
-            this.instance = new ConsoleLogger();
+        if(!this.logger){
+            this.logger = new ConsoleLogger();
         }
-        return this.instance;
+        return this.logger;
     }
 
     public static getInstance(){
         return this.createSingleton();
     }
+    log(...args: any[]): void{
+        console.log(...args);
+    };
+    end(): void{};
+    error(...args: any[]): void{
+        console.error(...args);
+    };
 }
